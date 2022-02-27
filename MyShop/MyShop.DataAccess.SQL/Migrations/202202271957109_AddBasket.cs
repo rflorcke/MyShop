4ce992ef960f80f5dd655e369a1a1d7f3eb158b6
Ryan Florcke <ryan.florcke@gmail.com>
2022-02-27 15:06:45 -0500

    Added Basket model

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202202271957109_AddBasket.cs
@@ -0,0 +1,45 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBasket : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        BasketId = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        Quantity = c.Int(nullable: false),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Baskets", t => t.BasketId)
+                .Index(t => t.BasketId);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AlterColumn("dbo.Products", "Name", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BasketItems", "BasketId", "dbo.Baskets");
+            DropIndex("dbo.BasketItems", new[] { "BasketId" });
+            AlterColumn("dbo.Products", "Name", c => c.String(maxLength: 20));
+            DropTable("dbo.Baskets");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
