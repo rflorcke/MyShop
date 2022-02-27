fccbc3340317b36411963653ca1f81c902427319
Ryan Florcke <ryan.florcke@gmail.com>
2022-02-27 13:04:49 -0500

    Added Image Upload

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -39,11 +40,17 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
                 return View(product);
+            }
+
+            if (file != null)
+            {
+                product.Image = product.Id + Path.GetExtension(file.FileName);
+                file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
             }
 
             context.Insert(product);
@@ -70,11 +77,11 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
 
-            if (product == null)
+            if (productToEdit == null)
             {
                 return HttpNotFound();
             }
@@ -84,9 +91,15 @@ namespace MyShop.WebUI.Controllers
                 return View(product);
             }
 
+            if (file != null)
+            {
+                productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+            }
+
             productToEdit.Category = product.Category;
             productToEdit.Description = product.Description;
-            productToEdit.Image = product.Image;
+            //productToEdit.Image = product.Image;
             productToEdit.Name = product.Name;
             productToEdit.Price = product.Price;
 
